2fc95d51fad36dbc937eef62c55b949440d5382a
dunieskysp <duniesky.salazar@gmail.com>
2025-05-03 18:18:55 -0400

    Actualizada las versiones de las dependencias

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_desc = Path("README.md").read_text("utf-8")
 # Datos del paquete
 setup(
     name="utilsdsp",
-    version="0.1.4",
+    version="0.1.5",
     author="Duniesky Salazar Pérez",
     author_email="<duniesky.salazar@gmail.com>",
     description="Algunas funciones útiles",
@@ -16,11 +16,11 @@ setup(
     url='https://github.com/dunieskysp/utils_dsp',
     packages=find_packages(),
     install_requires=[
-        "outputstyles==1.0.0",
-        "validators==0.28.1",
-        "requests==2.31.0",
-        "tqdm==4.66.2",
-        "curl_cffi==0.9.0"
+        "outputstyles>=1.0.0",
+        "validators>=0.28.1",
+        "requests>=2.31.0",
+        "tqdm>=4.66.2",
+        "curl_cffi>=0.9.0"
     ],
     keywords=['python', 'utilsdsp'],
     classifiers=[
